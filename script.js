c9903eb6241045938c9471b4a87170efa72f316a
Jose <joseferleal@gmail.com>
2023-03-18 19:02:39 -0500

    Clase 02 completada

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector("[data-form-btn]");
+const input=document.querySelector("[data-form-input]");
+const createTask=((evento)=>{
+    evento.preventDefault();
+    const input=document.querySelector("[data-form-input]");
+    console.log(input.value);
+
+})
+
+console.log(btn);
+
+//Arrow functions (funciones anónimas)
+btn.addEventListener("click", createTask);
