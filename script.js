e2f034ea8d1cabb2ad52ec1f0a4bdd4f2cd004f0
Jose <joseferleal@gmail.com>
2023-03-18 22:07:42 -0500

    Proyecto finalizado

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
-(()=>{ 
+
 const btn = document.querySelector("[data-form-btn]");
 const input=document.querySelector("[data-form-input]");
 
@@ -19,10 +21,11 @@ const createTask=((evento)=>{
     titleTask.classList.add("task")
     titleTask.innerText=value;
     taskContent.appendChild(titleTask);
-    const content= `<i class="fas fa-trash-alt trashIcon icon"></i>`;
+    
  // task.innerHTML=content;
 task.appendChild(taskContent);
-    list.appendChild(task);
+task.appendChild(deleteIcon());
+list.appendChild(task);
 
 
    
@@ -33,21 +36,4 @@ task.appendChild(taskContent);
 //Arrow functions (funciones anónimas)
 btn.addEventListener("click", createTask);
 
-const checkComplete=()=>{
-    const i = document.createElement("i");
-    i.classList.add("far", "fa-check-square", "icon");
-    i.addEventListener("click", completeTask);
-    return i
-};
 
-
-// Immediatly Invoked Function Expression IIFE
-const completeTask =(event)=>{
-
-    const element=event.target
-    element.classList.toggle("fas"); 
-    element.classList.toggle("completeIcon"); 
-    element.classList.toggle("far");
-
-}
-})();
